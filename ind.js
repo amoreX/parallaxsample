90a4406f7ca137677a5929352063603190158e8a
N!hal <71655709+amoreX@users.noreply.github.com>
2024-01-07 18:47:53 +0530

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,14 @@
+const paral = document.querySelectorAll(".parallax");
+
+let xVal = 0, yVal = 0;
+
+window.addEventListener("mousemove", (e) => {
+    xVal = e.clientX - window.innerWidth / 2;
+    yVal = e.clientY - window.innerHeight / 2;
+
+    console.log(xVal, yVal);
+    paral.forEach((item) => {
+		let speedx=item.dataset.speedx;
+        item.style.transform = `translateX(calc(-50% + ${-xVal*speedx}px)) translateY(calc(-50% + ${yVal*speedx}px))`;
+    });
+});
